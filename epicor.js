47ec9bf3b0458f3bc639804937b21d11b15ae4bd
asma gaini <asma.gaini@yahoo.com>
2023-09-13 10:33:46 +0430

    new creat with other way purchase count  and linked to counter

--- epicor.js
+++ epicor.js
@@ -1,18 +1,44 @@
+
+
 $(document).ready(function(){
-    var counter = 1;
-    // $(".Purchase-count").hide();
+    // var counter = 1;
+    $(".Purchase-count").hide();
     $(".breakfast_contant_info_price_btn").click(function(e){
-        // function afterText() {
-        //     var txt1 = "<button>- </button>";         
-        //     var txt2 = "<span>- </span>";
-        //     var txt3 = "<button>- </button>"; 
-        //     txt3.innerHTML = "jQuery!";
-        //     $(".breakfast_contant_info_price_btn").after(txt1, txt2, txt3); 
-        //   }
+       
         $(this).hide();
-        var section_count = '<div class="Purchase-count"><button class="btn-counter">-</button><span> 1 </span><button class="btn-counter">+</button></div>';
-        $(this).replaceWith(section_count);
-        // $(".Purchase-count").show();
-        // alert("salam");
+        var section_count = $('<div class="Purchase-count"><button class="btn-counter-Decrement">-</button><span class="span"> 1 </span><button class="btn-counter-Increment">+</button></div>');
+        $(this).after(section_count);
+        // alert("salam")
+        // var section_count = '<div class="Purchase-count"><button class="btn-counter-Decrement">-</button><span class="span"> 1 </span><button class="btn-counter-Increment">+</button></div>';
+        // $(this).replaceWith(section_count);
+        var counter_val = $(".span").html();
+        var counter = parseInt(counter_val);
+        console.log(counter);
+        
+        $(".btn-counter-Increment").click(function(e){
+            counter++;
+            console.log(counter);
+            $(".span").html(" "+counter+" ");
+        })
+
+        $(".btn-counter-Decrement").click(function(e){
+            counter--;
+            if (counter < 1) {
+                // var btn = '<button class="breakfast_contant_info_price_btn">افزودن ب دفترچه</button>'
+                // $(section_count).replaceWith(btn);
+
+                // const new_button = $("<div>");
+                // $(new_button).html('<button class="breakfast_contant_info_price_btn">افزودن ب دفترچه</button>');
+                // $(".Purchase-count").append(new_button);
+                // alert("salam")
+                $(".Purchase-count").hide();
+
+
+                $(".breakfast_contant_info_price_btn").show();
+                console.log(new_button)
+            }
+            console.log(counter);
+            $(".span").html(" "+counter+" ");
+        })
     })
 });
