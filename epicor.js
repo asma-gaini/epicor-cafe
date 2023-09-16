fe00437833f879c420af31dd1abe67e6c435b75e
asma gaini <asma.gaini@yahoo.com>
2023-09-16 14:12:00 +0430

    Fixing the number of orders bug and arranging the file and code of jquery

--- epicor.js
+++ epicor.js
@@ -1,73 +1,50 @@
+var mainBtnClass = 'breakfast_contant_info_price_btn';
+var pay = 0;
 
 $(document).ready(function(){
-    // var pay = parseInt($(".pay_counter").html());
-    var pay = 0;
-    console.log("tarif pay = "+pay);
-    $(".Purchase-count").hide();
-    $(".breakfast_contant_info_price_btn").on("click" ,function(e){
-       
-        $(this).hide();
-        var span = "menu = '" + $(this).attr("menu") + "'"
-        console.log(span);
-        var section_count = $('<div class="Purchase-count"><button class="btn-counter-Decrement">-</button><span class="span"'+span+'> 1 </span><button class="btn-counter-Increment">+</button></div>');
-        $(this).after(section_count);
-
-        var temp = $(this).attr("menu");
-        console.log("temp= " + temp);
-
-        var spanContantCounter = parseInt($(this).siblings("div").find("span").html());
-        console.log("spanContantCounter= " + spanContantCounter);
-
-        var price = parseInt($(this).parent().siblings("div").find(".price").html());
-        console.log("price = "+price);
-        pay += price;
-
-        $(".pay_counter").html(pay + "/000");
-        
-        $(".btn-counter-Increment").click(function(e){
-            // spanContantCounter = $(this).parent().find("span").html();
-
-            spanContantCounter = $(this).siblings("span").html();
-// debugger
-            console.log(" ghbl ezafe kardan = "+spanContantCounter);
-            spanContantCounter++;
-            // $(this).parent().find("span").html(" "+spanContantCounter+" ");
-            $(this).siblings("span").html(" "+spanContantCounter+" ");
-            console.log("meghdar span bade ezafe"+$(this).siblings("span").html());
-
-
-            // price = parseInt($(this).parent().parent().siblings("div").find(".price").html());
-            // console.log(price)
-            // console.log("pay ghabl = "+pay);
-            // console.log("price = "+price);
-            // pay += price;
-            // console.log("price = "+price);
-            // console.log("pay bad = "+pay);
-            // $(".pay_counter").html(pay + "/000");
-        })
-
-        $(".btn-counter-Decrement").click(function(e){
-            spanContantCounter = $(this).siblings("span").html();
-            console.log(" ghbl kam kardan = "+spanContantCounter);
-            spanContantCounter--;
-            if (spanContantCounter < 1) {
-                $(this).parent().hide();
-                $(".pay_counter").html("0");
-                $(this).parent().parent().find("button").show();
-            }
-            $(this).siblings("span").html(" "+spanContantCounter+" ");
-            console.log("meghdar span bade kam" + $(this).siblings("span").html());
-
-
-            // price = parseInt($(this).parent().parent().siblings("div").find(".price").html());
-            // pay -= price;
-            // console.log(pay);
-            // $(".pay_counter").html(pay + "/000");
-        })
-
-    })
 });
 
 
+function AddToBreakfast(customAttr)
+{
+	var myHtmlTag = $("button." +mainBtnClass+"[menuID='"+customAttr+"']");
+	myHtmlTag.hide();
+    var span = "menuID = '" + myHtmlTag.attr("menuID") + "'";
+	var menuIDContent = myHtmlTag.attr("menuID");
+	var increament_onclick = "onclick=Increament('span','"+menuIDContent+"')";
+	var decreament_onclick = "onclick=Decreament('span','"+menuIDContent+"')";
+    var section_count = $('<div class="Purchase-count" menuID="'+menuIDContent+'"><button class="btn-counter-Decrement"'+decreament_onclick+'>-</button><span class="span"'+span+'> 1 </span><button class="btn-counter-Increment" '+increament_onclick+'  >+</button></div>');
+    myHtmlTag.after(section_count);
 
-// che bara addad che bara gheymat az yeki ru in yeki miam  yedone az ghabl ezafe karde
+
+	
+}
+
+
+function Increament(customClass,customAttr)
+{
+	var increament_counter = $("span."+customClass+"[menuID='"+customAttr+ "']");
+	var counter = increament_counter.html();
+	counter++;
+	increament_counter.html(" "+counter+" ");
+	
+}
+
+function Decreament(customClass,customAttr)
+{
+	var decreament_counter = $("span."+customClass+"[menuID='"+customAttr+ "']");
+	var counter = decreament_counter.html();
+	counter--;
+	
+	if(counter < 1)
+	{
+		var parentDiv = $("div.Purchase-count[menuID='"+customAttr+ "']");
+		parentDiv.hide();
+		var mainButton = $("button."+mainBtnClass+"[menuID='"+customAttr+"']");
+		mainButton.show();
+	}
+	
+	decreament_counter.html(" "+counter+" ");
+}
+
+
