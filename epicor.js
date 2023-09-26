e1a34ba7b833864d7794166f57732ea1d7bea2e0
asma gaini <asma.gaini@yahoo.com>
2023-09-26 13:02:24 +0330

    scrolling menu

--- epicor.js
+++ epicor.js
@@ -31,7 +31,34 @@ $(document).ready(function(){
 	if (pay == 0) {
 		$("div.pay").hide();
 	}
+	// scrollMenu();
+
 });
+
+function scrollMenu(){
+	Draggable.create(".menu-bar", {
+		bounds:"#dragSpace",
+  //allowNativeTouchScrolling:false,
+		type:"x"
+		}
+	)
+
+	TweenMax.set("body", {overflowY:'scroll'});
+
+	var scrollSpeed = 0.5; // half of user scroll
+	var scrollHeight = 10000;
+
+	TweenMax.set("body", {height:scrollHeight, overflowY:'scroll'});
+
+	$(document).on("scroll", function (){
+  		const pixels = $(document).scrollTop();
+  		TweenMax.to('#wrapperBoxes',1,{x:-0.5 * pixels})
+  
+  //  '0.5' = half speed of user scroll
+  		console.log(pixels);
+	})
+
+}
 
 
 function linked(dataInfo){
