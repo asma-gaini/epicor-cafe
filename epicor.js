6280174e733b21ab37f116df2e95a36a6f160d1e
asma gaini <asma.gaini@yahoo.com>
2023-09-27 20:41:00 +0330

    fixe scroll bug

--- epicor.js
+++ epicor.js
@@ -61,6 +61,37 @@ function scrollMenu(){
 }
 
 
+/////////scroll new///
+const slider = document.querySelector('.items');
+let isDown = false;
+let startX;
+let scrollLeft;
+
+slider.addEventListener('mousedown', (e) => {
+  isDown = true;
+  slider.classList.add('active');
+  startX = e.pageX - slider.offsetLeft;
+  scrollLeft = slider.scrollLeft;
+});
+slider.addEventListener('mouseleave', () => {
+  isDown = false;
+  slider.classList.remove('active');
+});
+slider.addEventListener('mouseup', () => {
+  isDown = false;
+  slider.classList.remove('active');
+});
+slider.addEventListener('mousemove', (e) => {
+  if(!isDown) return;
+  e.preventDefault();
+  const x = e.pageX - slider.offsetLeft;
+  const walk = (x - startX) * 3; //scroll-fast
+  slider.scrollLeft = scrollLeft - walk;
+  console.log(walk);
+});
+
+
+
 function linked(dataInfo){
 	$("button.menu-bar_section").removeClass("menu-bar_section_click");
 	$("button.menu-bar_section"+"[data-info ='"+dataInfo+"']").addClass("menu-bar_section_click");
