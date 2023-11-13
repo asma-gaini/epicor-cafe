b830541c6b4688848a89ded58aebe4f3743e1735
asma gaini <asma.gaini@yahoo.com>
2023-11-13 18:15:53 +0330

    create general json

---
+++ js/general variable/general.js
@@ -0,0 +1,5 @@
+var logo = "./img/general image/logo.jpg";
+var brandNameEn = "Epicor cafe";
+var berandNameFa = "کافه اپیکور";
+var noImageFood = "./img/general image/noimagefood.png";
+var unavailableFood = "./img/general image/unavailable.png";
